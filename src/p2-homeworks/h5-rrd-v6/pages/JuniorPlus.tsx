cccfc17c7416389e9ebc1d1769ec52aeab2f3130
jiemoh <jiemoh@list.ru>
2022-10-12 20:56:03 +0300

    added HW5

--- src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-function Header() {
+function JuniorPlus() {
     return (
         <div>
-            // add NavLinks
+
+            {/*<HW12/>*/}
 
         </div>
     )
 }
 
-export default Header
+export default JuniorPlus
